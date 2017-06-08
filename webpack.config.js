1b81a03b12336ec98838a7164c1b98939c233a79
yan358941877 <yan358941877@163.com>
2017-06-09 00:05:21 +0800

    测试基本框架

--- webpack.config.js
+++ webpack.config.js
@@ -42,7 +42,7 @@ module.exports = {
           use: ['css-loader'],
           fallback: 'style-loader'
         }),
-        include: /node_modeles/
+        include: /node_modules/
       },
       {
         test: /\.(jpg|png)$/,
