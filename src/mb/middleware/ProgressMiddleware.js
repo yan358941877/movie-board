c3bb5593dcdf98d66a7c4af8105f27bc2c81b564
yan358941877 <yan358941877@163.com>
2017-06-10 16:47:24 +0800

    自定义中间件

---
+++ src/mb/middleware/ProgressMiddleware.js
@@ -0,0 +1,26 @@
+import actionCreators from '../actions/status-action-creators'
+
+const loadingActions = []
+
+export default store => next => action =>{
+    if(action.payload instanceof Promise){
+        if(action.type === 'LOAD_SUBJECT'){
+            loadingActions.splice(0, loadingActions.length)
+        }
+        loadingActions.push(action.type)
+        if(!store.getState().getIn(['status','isLoading'])){
+            store.dispatch(actionCreators.load())
+        }
+    }
+
+    else {
+        const index = loadingActions.indexOf(action.type)
+        if(index !== -1){
+            loadingActions.splice(index, 1)
+            if(loadingActions.length === 0 && store.getState().getIn(['status', 'isLoading'])){
+                store.dispatch(actionCreators.loadComplete())
+            }
+        }
+    }
+    next(action)
+}
